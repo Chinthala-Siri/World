a92b29bf3d8a0726cc7be85bb83e74fc45e7b79c
Chinthala-Siri <sirichinthala741@gmail.com>
2025-11-20 15:52:03 +0530

    Add files via upload

---
+++ swapnum.java
@@ -0,0 +1,11 @@
+class swapnum{
+publilc static void main (String args[]){
+a=10;
+b=20;
+System.out.println("Before Swapping a=" +a+ "b=" +b);
+int temp=a;
+a=b;
+b=temp;
+System.out.println("After Swapping a=" +a+ "b=" +b);
+}
+}
